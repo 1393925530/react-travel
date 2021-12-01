a93f460ae9adaba6554f0def137790ad6c46e592
1393925530 <zzc1393925530@gmail.com>
2021-12-01 10:31:30 +0800

    ✨ feat(路由): 基础路由系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Result } from 'antd'
 import { HomePage } from './pages'
 
 import styles from './App.module.css'
 
-function App() {
+const App = () => {
   return (
     <div className={styles.App}>
       <BrowserRouter>
-        <Route path="/" component={HomePage} />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/signIn" render={() => <h1>登录</h1>} />
+          <Route
+            render={() => (
+              <Result
+                className={styles.result}
+                status="404"
+                title="404"
+                subTitle="Sorry, the page you visited does not exist."
+              />
+            )}
+          />
+        </Switch>
       </BrowserRouter>
     </div>
   )
