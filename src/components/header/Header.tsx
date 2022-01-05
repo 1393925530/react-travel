cb23a686f910d57d59d5f2b509d25c3b1dd8b0fa
1393925530 <zzc1393925530@gmail.com>
2022-01-05 14:18:36 +0800

    ✨ feat(redux): 创建state

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -35,16 +35,18 @@ export const Header: React.FC = () => {
             语言
           </Dropdown.Button>
           <Button.Group className={styles['button-group']}>
-            <Button>注册</Button>
-            <Button>登陆</Button>
+            <Button onClick={() => history.push('register')}>注册</Button>
+            <Button onClick={() => history.push('signIn')}>登陆</Button>
           </Button.Group>
         </div>
       </div>
       <Layout.Header className={styles['main-header']}>
-        <img src={logo} alt="" className={styles['App-logo']} />
-        <Typography.Title level={3} className={styles.title}>
-          React 旅游网
-        </Typography.Title>
+        <span onClick={() => history.push('/')}>
+          <img src={logo} alt="" className={styles['App-logo']} />
+          <Typography.Title level={3} className={styles.title}>
+            React 旅游网
+          </Typography.Title>
+        </span>
         <Input.Search
           placeholder="请输入旅游目的地、主题或者关键字"
           className={styles['search-input']}
