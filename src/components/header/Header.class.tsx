1ae9ddcd8e8b951bf7d0c7444924bb0f8ec6bbf5
1393925530 <zzc1393925530@gmail.com>
2022-01-05 14:38:15 +0800

    ✨ feat(redux): 访问state

--- src/components/header/Header.class.tsx
+++ src/components/header/Header.class.tsx
@@ -3,10 +3,22 @@ import logo from '../../assets/logo.svg'
 import { Menu, Dropdown, Input, Layout, Typography, Button } from 'antd'
 import { GlobalOutlined } from '@ant-design/icons'
 import { withRouter, RouteComponentProps } from 'react-router-dom'
+import store from '../../redux/store'
+import { LanguageState } from '../../redux/languageReducer'
 
 import styles from './Header.module.css'
 
-class HeaderComponent extends React.Component<RouteComponentProps> {
+interface State extends LanguageState {}
+
+class HeaderComponent extends React.Component<RouteComponentProps, State> {
+  constructor(props) {
+    super(props)
+    const storeState = store.getState()
+    this.state = {
+      language: storeState.language,
+      languageList: storeState.languageList,
+    }
+  }
   render() {
     const { history } = this.props
     return (
@@ -19,13 +31,14 @@ class HeaderComponent extends React.Component<RouteComponentProps> {
               style={{ marginLeft: 15 }}
               overlay={
                 <Menu>
-                  <Menu.Item>中文</Menu.Item>
-                  <Menu.Item>ENGLISH</Menu.Item>
+                  {this.state.languageList.map((l) => {
+                    return <Menu.Item key={l.code}>{l.name}</Menu.Item>
+                  })}
                 </Menu>
               }
               icon={<GlobalOutlined />}
             >
-              语言
+              {this.state.language === 'zh' ? '中文' : 'English'}
             </Dropdown.Button>
             <Button.Group className={styles['button-group']}>
               <Button onClick={() => history.push('register')}>注册</Button>
