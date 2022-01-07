47893bba63aab1ae8dfc44a05a1365bc90a99ff3
1393925530 <zzc1393925530@gmail.com>
2022-01-07 10:17:12 +0800

    ✨ feat(axios): Ajax异步获取数据

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,10 @@ import App from './App'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import './i18n/configs'
+import axios from 'axios'
 import 'antd/dist/antd.css'
+
+axios.defaults.headers['x-icode'] = 'mock'
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -7,22 +7,72 @@ import {
   ProductCollection,
   CollaborateCompany,
 } from '../../components'
-import { Row, Col, Typography } from 'antd'
-import {
-  productList1,
-  productList2,
-  productList3,
-} from '../../pages/home/mockups'
+import { Row, Col, Typography, Spin } from 'antd'
 import { withTranslation, WithTranslation } from 'react-i18next'
+import axios from 'axios'
 import sideImage from '../../assets/images/sider_2019_12-09.png'
 import sideImage2 from '../../assets/images/sider_2019_02-04.png'
 import sideImage3 from '../../assets/images/sider_2019_02-04-2.png'
 
 import styles from './HomePage.module.css'
 
-class HomePageComponent extends React.Component<WithTranslation> {
+interface State {
+  loading: boolean
+  error: string | null
+  productList: any[]
+}
+
+class HomePageComponent extends React.Component<WithTranslation, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true,
+      error: null,
+      productList: [],
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get(
+        ` http://192.168.120.194:7300/mock/61d79dc72c7cf895bc0c49e1/productCollections`
+      )
+      this.setState({
+        loading: false,
+        error: null,
+        productList: data.data,
+      })
+    } catch (error: any) {
+      this.setState({
+        error: error.message,
+        loading: false,
+      })
+    }
+  }
+
   render() {
     const { t } = this.props
+    const { productList, loading, error } = this.state
+
+    if (loading) {
+      return (
+        <Spin
+          size="large"
+          style={{
+            marginTop: 200,
+            marginBottom: 200,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            width: '100%',
+          }}
+        />
+      )
+    }
+
+    if (error) {
+      return <div>网站出错：{error}</div>
+    }
+
     return (
       <div className={styles.App}>
         <Header />
@@ -43,7 +93,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage}
-            products={productList1}
+            products={productList[0].touristRoutes}
           ></ProductCollection>
           <ProductCollection
             title={
@@ -52,7 +102,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage2}
-            products={productList2}
+            products={productList[1].touristRoutes}
           ></ProductCollection>
           <ProductCollection
             title={
@@ -61,7 +111,7 @@ class HomePageComponent extends React.Component<WithTranslation> {
               </Typography.Title>
             }
             sideImage={sideImage3}
-            products={productList3}
+            products={productList[2].touristRoutes}
           ></ProductCollection>
           <CollaborateCompany />
         </div>
